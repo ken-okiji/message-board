9f22867647b0f92a80de44dfbc8c6756eeaa25dd
ken_okiji <no_sweet_without_sweat@i.softbank.jp>
2015-10-05 03:34:12 +0000

    Implementation of the validation

--- memo.rb
+++ memo.rb
@@ -88,3 +88,12 @@ private
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
     params.require(:message).permit(:name, :body)
 end
+
+#バリデーション
+オブジェクトがデータベースに保存される前に、そのデータが正しいかどうかを検証する仕組み
+class Message < ActiveRecord::Base
+    # 名前は必須入力かつ20文字以内
+    validates :name , length: {  maximum: 20 } , presence: true
+    # 内容は必須入力かつ2文字以上30文字以下
+    validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
+end
